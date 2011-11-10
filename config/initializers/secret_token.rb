f27ffae294520271ee1ece686b0abba2e2bef41b
Kenichi Yorozu <rozken@gmail.com>
2011-11-10 16:07:54 +0900

    added source codes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxPostTest::Application.config.secret_token = 'ef5c2ddf16881310557f173cf1110c165ba21bc81a544e0527cf7e988a45c4daa6e890df97c775cb21848a19d7093dc8d94df43fbfbf8a35f44a0c5dc1f2485f'
